9b4a93249e547f6c1c89b906bce5fc3bf999137e
Theo <theokoss2002@gmail.com>
2024-04-04 14:44:19 -0500

    Added CSV Output for matrices

--- FMatrix.jl
+++ FMatrix.jl
@@ -1,9 +1,14 @@
 include("Main.jl")
+using CSV
+using DataFrames
+using DelimitedFiles
 function GetMatrix(rows::Int,cols::Int)
     A = zeros(Int, rows,cols)
     for i = 1:rows for j = 1:cols
             A[i,j]=F(i,j)
         end
     end
+    FileName = "$rows X $cols.csv"
+    CSV.write(FileName, Tables.table(A),delim=',',decimal='.')
     return A
 end
